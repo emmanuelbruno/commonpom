c554477e720ccc4c32588fe49f089edf8ecde9d8
Emmanuel Bruno <emmanuel.bruno@univ-tln.fr>
2017-09-28 22:44:33 +0200

    fixes mvn docker script

--- .travis.scripts/mvn.sh
+++ .travis.scripts/mvn.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 docker run \
        --rm \
-       -v $HOME/.m2:/home/user/.m2/repository \
+       -v $HOME/.m2:/home/user/.m2 \
        -v $(pwd):/usr/src/mysrc \
        -w /usr/src/mysrc \
        maven:3.3.9-jdk-8-alpine \
