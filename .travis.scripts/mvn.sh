dc84f8e519b516d581ec5c187ae11c418f0363ed
Emmanuel Bruno <emmanuel.bruno@univ-tln.fr>
2017-09-29 11:52:23 +0200

    typo

--- .travis.scripts/mvn.sh
+++ .travis.scripts/mvn.sh
@@ -4,7 +4,7 @@ docker run \
        -v $HOME/.m2:/home/user/.m2 \
        -v $(pwd):/usr/src/mysrc \
        -w /usr/src/mysrc \
-       -s /home/user/.m2/settings.xml \
        maven:3.3.9-jdk-8-alpine \
        mvn \
+       -s /home/user/.m2/settings.xml \       
        -Duser.home=/home/user $*
