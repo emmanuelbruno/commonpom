2d80f97e99e379b1af13e1d599c30ae9a6c92f0f
Emmanuel Bruno <emmanuel.bruno@univ-tln.fr>
2017-09-29 12:29:55 +0200

    cleans user directories in docker and sets sonar cache.

--- .travis.scripts/mvn.sh
+++ .travis.scripts/mvn.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 docker run \
        --rm \
-       -v $HOME/.m2:/home/user/.m2 \
+       -v $HOME/.sonar/cache:$USER_HOME_DIR/.sonar/cache
+       -v $HOME/.m2:$USER_HOME_DIR/.m2 \
        -v $(pwd):/usr/src/mysrc \
        -w /usr/src/mysrc \
        maven:3.3.9-jdk-8-alpine \
        mvn \
-       -Duser.home=/home/user \
-       --settings /home/user/.m2/settings.xml \
+       -Duser.home=$USER_HOME_DIR \
+       --settings $USER_HOME_DIR/.m2/settings.xml \
        $*
