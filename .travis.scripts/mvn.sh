035221deab0a38f6d22428ee6e2b035da85363cc
Emmanuel Bruno <emmanuel.bruno@univ-tln.fr>
2017-09-29 18:14:43 +0200

    typos.

--- .travis.scripts/mvn.sh
+++ .travis.scripts/mvn.sh
@@ -5,11 +5,11 @@ docker run \
        -e SONAR_TOKEN=${SONAR_TOKEN} \
        -e BINTRAY_API_KEY=${BINTRAY_API_KEY} \
        -v $HOME/.sonar/cache:${USER_HOME_DIR}/.sonar/cache \
-       -v $HOME/.m2:{$USER_HOME_DIR}/.m2 \
+       -v $HOME/.m2:${USER_HOME_DIR}/.m2 \
        -v $(pwd):/usr/src/mysrc \
        -w /usr/src/mysrc \
        maven:3.5.0-jdk-8-alpine \
        mvn \
-       -Duser.home=$USER_HOME_DIR \
-       --settings $USER_HOME_DIR/.m2/settings.xml \
+       -Duser.home=${USER_HOME_DIR} \
+       --settings ${USER_HOME_DIR}/.m2/settings.xml \
        $*
