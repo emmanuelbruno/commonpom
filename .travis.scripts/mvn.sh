03631c0104de2536d52bb119d36e5029173af51d
Emmanuel Bruno <emmanuel.bruno@univ-tln.fr>
2017-09-30 11:24:02 +0200

    passes utln password variable to docker container

--- .travis.scripts/mvn.sh
+++ .travis.scripts/mvn.sh
@@ -4,6 +4,8 @@ docker run \
        --rm \
        -e SONAR_TOKEN=${SONAR_TOKEN} \
        -e BINTRAY_API_KEY=${BINTRAY_API_KEY} \
+       -e UTLN_EMAIL=${UTLN_EMAIL} \
+       -e UTLN_PASSWORD=${UTLN_PASSWORD} \
        -v $HOME/.sonar/cache:${USER_HOME_DIR}/.sonar/cache \
        -v $HOME/.m2:${USER_HOME_DIR}/.m2 \
        -v $(pwd):/usr/src/mysrc \
