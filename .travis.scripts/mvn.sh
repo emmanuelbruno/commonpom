730a2ab6ab7ca570087a8f0dc5e55fe6c8cfe8c9
Emmanuel Bruno <emmanuel.bruno@univ-tln.fr>
2018-10-24 11:05:51 +0200

    upgrades maven version.

--- .travis.scripts/mvn.sh
+++ .travis.scripts/mvn.sh
@@ -11,7 +11,7 @@ docker run \
        -v $HOME/.m2:${USER_HOME_DIR}/.m2 \
        -v $(pwd):/usr/src/mysrc \
        -w /usr/src/mysrc \
-       maven:3.5.0-jdk-8-alpine \
+       maven:3.5.4-jdk-8-alpine \
        mvn \
        -Duser.home=${USER_HOME_DIR} \
        --settings ${USER_HOME_DIR}/.m2/settings.xml \
